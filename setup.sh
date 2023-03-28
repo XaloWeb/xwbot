71fb5049fba5252d9f3447d65b71a89ed68a56dd
XaloWeb <129140943+XaloWeb@users.noreply.github.com>
2023-03-28 22:53:05 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,17 @@
 
+sudo apt update 
+sudo apt install curl 
+sudo apt install tmux
+sudo curl -sSL https://deb.nodesource.com/setup_18.x | sudo bash - 
+sudo apt install -y nodejs 
+sudo npm install pnpm -g
+sudo git clone https://github.com/XaloWeb/xwbot.git 
+cd xwbot/ 
+sudo pnpm bootstrap 
+cd service/ 
+sudo pnpm install 
+cd  
+cd xwbot/ 
+sudo bash start.sh
+tmux new-session -s mysession 
+start.sh 
