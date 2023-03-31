62fa9b8c9ef48b202410a45c8a664e78496b07ad
XaloWeb <129140943+XaloWeb@users.noreply.github.com>
2023-03-31 15:29:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 apt  install -y docker-compose
 sudo curl -sSL https://deb.nodesource.com/setup_18.x | sudo bash - 
 sudo apt install -y nodejs 
-sudo apt install tmux
-sudo npm install pnpm -g
+sudo apt install -y tmux
+sudo npm install -y pnpm -g 
 mkdir /var/www
 cd /var/www
 sudo git clone https://github.com/XaloWeb/xwbot.git
@@ -15,5 +15,6 @@ cd
 cd /var/www/html/docker-compose
 docker-compose up -d 
 cd /var/www/html 
-sudo tmux new-session -s mysession 
+sudo tmux new-session -s mysession
+
 sudo bash start.sh 
